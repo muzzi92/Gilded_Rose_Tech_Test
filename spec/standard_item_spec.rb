1373cfc1ff344bd324e9bdc3f907bcf53af207d6
Muzzi Aldean <amiraldean@gmail.com>
2018-06-22 15:04:10 +0100

    unit tested all inherited attributes for standard_item class

--- spec/standard_item_spec.rb
+++ spec/standard_item_spec.rb
@@ -5,5 +5,11 @@ describe StandardItem do
     it 'Inherits name attribute from Item parent class' do
       expect(subject.name).to eq('Axe')
     end
+    it 'Inherits sell_in attribute from Item parent class' do
+      expect(subject.sell_in).to eq(10)
+    end
+    it 'Inherits quality attribute from Item parent class' do
+      expect(subject.quality).to eq(20)
+    end
   end
 end
