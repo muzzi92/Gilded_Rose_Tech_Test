cc4810adde2ae6896170eef1a312493ff335edff
Muzzi Aldean <amiraldean@gmail.com>
2018-06-22 12:31:09 +0100

    Utilised spy in unit test to mock item

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -1,12 +1,16 @@
 require 'gilded_rose'
 
 describe GildedRose do
-  let(:standard_item) { double :item, name: 'Elixir of the Mongoose' }
-  subject { GildedRose.new([standard_item]) }
+  let(:item) { spy :item }
+  subject { GildedRose.new([item]) }
 
   describe '#update_quality' do
     it 'Calls update_standard_item' do
-      expect(subject).to receive(:update_standard_item).with(standard_item)
+      expect(subject).to receive(:update_standard_item).with(item)
+      subject.update_quality
+    end
+    it ' Calls update_brie' do
+      expect(subject).to receive(:update_brie).with(item)
       subject.update_quality
     end
   end
