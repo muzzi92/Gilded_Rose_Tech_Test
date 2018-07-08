a4a6208991d696afe67ffdb6fcc6e38101d6412e
Muzzi Aldean <amiraldean@gmail.com>
2018-07-08 13:40:36 +0100

    Added update_item override function for sulfuras that has no affect

--- lib/sulfuras.rb
+++ lib/sulfuras.rb
@@ -2,4 +2,8 @@ class Sulfuras < StandardItem
   def initialize(name = 'Sulfuras, Hand of Ragnaros', sell_in, quality)
     super
   end
+
+  def update_item
+  end
+  
 end
--- spec/sulfuras_spec.rb
+++ spec/sulfuras_spec.rb
@@ -3,7 +3,7 @@ require 'sulfuras'
 describe Sulfuras do
 
   subject { Sulfuras.new(10, 20) }
-  
+
   describe '#initialize' do
     it 'Inherits sell_in attribute from StandardItem class' do
       expect(subject.sell_in).to eq(10)
@@ -13,6 +13,17 @@ describe Sulfuras do
     end
     it 'Initializes with a default name attribute of Sulfuras, Hand of Ragnaros' do
       expect(subject.name).to eq('Sulfuras, Hand of Ragnaros')
+    end
+  end
+
+  describe '#update_item' do
+    it 'Has no affect on sell_in' do
+      subject.update_item
+      expect(subject.sell_in).to eq(10)
+    end
+    it 'Has no affect on quality' do
+      subject.update_item
+      expect(subject.quality).to eq(20)
     end
   end
 
